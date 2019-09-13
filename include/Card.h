8b67f435adfca148a0bf11c53460120436bb18ea
Steven (Steven) Deutekom <deus3720@36torch.cs.uleth.ca>
2019-09-13 14:41:51 -0600

    Added documentation to Card class. Moved some other files around and created some files for Hand class.

--- include/Card.h
+++ include/Card.h
@@ -1,18 +1,45 @@
+/**
+ * 3720 Individual project card game.
+ * @author Steven Deutekom
+ * @date sept 13, 2019
+ */
 #ifndef CARD_H
 #define CARD_H
 
 
 #include "Suit.h"
 
+/**
+ * A standard suited playing card.
+ */
 class Card {
   public:
-   Card(int v, Suit s) : value(v), suit(s) {};
-   int getValue() const {return value;}
-   int getSuit() const {return suit;}
+    /**
+     * Creates a new card.
+     *
+     * @param v The value of the card. Must be in range [1, 13].
+     * @param s The suit of the card. Must be a valid Suit.
+     * @Throws InvalidParameterError if v or s are not valid.
+     */
+    Card(int v, Suit s) : value(v), suit(s) {};
+
+    /**
+     * Returns the value of the card.
+     * 
+     * @return the cards value.
+     */
+    int getValue() const {return value;}
+
+    /**
+     * Returns the suit of the card.
+     *
+     * @return the cards suit.
+     */
+    int getSuit() const {return suit;}
 
   private:
-   int value;
-   Suit suit;
+    int value;
+    Suit suit;
 };
 
 #endif
